368906c7d5b6c5ec4d323a24f8c9e77576255857
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-08-09 23:09:49 +0300

    loggers created

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const routes = require('./routes');
 const { login, signUp } = require('./controllers/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 
-const { PORT = 3000 } = process.env;
+const { PORT, dbURL } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/news-explorerdb', {
+mongoose.connect(dbURL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -20,14 +22,31 @@ mongoose.connect('mongodb://localhost:27017/news-explorerdb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', signUp);
+app.use(requestLogger);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), signUp);
 
 app.use(auth);
 app.use('/', routes);
 
+app.use(errorLogger);
+
+app.use(errors());
+
 app.use((err, req, res, next) => {
-  res.status(err.statusCode || 500).send({ message: err.message || 'На сервере произошла ошибка' });
+  res.status(err.statusCode || 500).send({ message: err.message || 'Server Error' });
   next();
 });
 
